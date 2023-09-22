32beb3b1bbeafcde6d164c466b44495d42791df7
minky <minkyeong02@daum.net>
2023-09-23 00:22:18 +0900

    [문자열] 팰린드롬수

---
+++ src/Main_1259.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+//[문자열] 팰린드롬수
+public class Main_1259 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        while (true){
+            String s1 = s.nextLine();
+            boolean isPalindrome = true;
+            if (s1.equals("0")) break;
+
+            char [] word = s1.toCharArray();
+            int i=0;
+            int j=word.length-1;
+            while (i<j) {
+                if (word[i] == word[j]){
+                    i++;
+                    j--;
+                } else {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome) System.out.println("yes");
+            else System.out.println("no");
+
+        }
+    }
+}
