d61243971187e704757cd9deb3dbbf2f34ffbd6a
minky <minkyeong02@daum.net>
2023-11-24 19:03:17 +0900

    [정렬] 숫자놀이

--- src/Main_1755.java
+++ src/Main_1755.java
@@ -21,9 +21,10 @@ public class Main_1755 {
         alp.add("seven");
         alp.add("eight");
         alp.add("nine");
+        //알파벳 초기화
 
         String [] number = new String[N-M+1];
-        int [] num = new int[N-M+1];
+        int [] num = new int[N-M+1]; //알파벳과 숫자를 다루기
         int index=0;
         for (int i=M; i<=N; i++){
             num[index] = i;
@@ -33,11 +34,14 @@ public class Main_1755 {
             }
             number[index] = number[index].concat(alp.get(i%10));
             index++;
-        }
+        } //숫자에 해당하는 문자들을 가져와서 넣어준다
+        //만약 8 이라면 십의 자리는 없으니까 패스, 일의 자리에 해당하는 8 "eight"만 가져와서 concat
+        //만약 20 이라면 십의 자리는 2라서 먼저 two를 넣고, 일의 자리에 해당하는 0 "zero"를 가져와서 concat -> twozero
 
         for (int i=0; i<=N-M; i++){
-            for (int j=0; j<=N-M-1-i; j++){
-                if (number[j].compareTo(number[j+1])>0){
+            for (int j=0; j<=N-M-1-i; j++){ //버블 정렬로 정리, 문자를 기준으로 오름차순 정렬하면서 숫자들도 같이 옮겨준다.
+                if (number[j].compareTo(number[j+1])>0){ //compareTo는 호출하는 문자가 사전적으로 앞이면 음수
+                                                        //매개변수가 사전적으로 앞이면 양수, 동일하면 0
                     String tmp = number[j+1];
                     number[j+1] = number[j];
                     number[j] = tmp;
@@ -49,7 +53,7 @@ public class Main_1755 {
         }
         int idx=0;
         for (int i=1; i<=N-M+1; i++) {
-            if (i%10 == 0) System.out.println(num[idx]);
+            if (i%10 == 0) System.out.println(num[idx]); //10개씩 끊어서 출력
             else System.out.print(num[idx]+" ");
             idx++;
         }
